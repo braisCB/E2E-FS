2d644d7dec513a6fec1ddaa50763bac80ab16f13
Brais <braisso@gmail.com>
2021-01-23 17:24:35 +0100

    new results

---
+++ src/baseline_methods/LASSORFE.py
@@ -0,0 +1,34 @@
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import Lasso
+import numpy as np
+from keras.utils import to_categorical
+
+
+class LASSORFE:
+
+    def __init__(self, n_features_to_select=10, normalize=True, step=.001):
+        self.n_features_to_select = n_features_to_select
+        self.score = None
+        self.ranking = None
+        self.normalize = normalize
+        self.step = step
+
+    def fit(self, X, y):
+        new_X = X.copy()
+        if self.normalize:
+            new_X -= new_X.min(axis=0)
+            new_X /= new_X.max(axis=0)
+        estimator = Lasso(alpha=1.)
+        print('nfeatures', self.n_features_to_select)
+        self.selector = RFE(estimator, n_features_to_select=self.n_features_to_select, step=self.step).fit(new_X, y)
+        self.score = 1. / self.selector.ranking_
+        self.ranking = self.selector.ranking_
+        print((self.ranking == 1).sum())
+
+    def transform(self, X):
+        new_X = X[:, self.ranking[:self.n_features_to_select]]
+        return new_X
+
+    def fit_transform(self, X, y):
+        self.fit(X, y)
+        return self.transform(X)
